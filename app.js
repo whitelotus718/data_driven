d4a46080d274159cdc3d53407a0b4a1f3c597ea4
Marc Carlson <marc.carlson30@gmail.com>
2021-01-22 12:06:20 -0600

    require morgan

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
+const morgan = require('morgan');
 const routes = require('./routes');
 
 const app = express();
 
 app.set('view engine', 'pug');
-
+app.use(morgan('dev'));
 app.use(routes); // is this being used for something other than a middleware?
 
 module.exports = app;
