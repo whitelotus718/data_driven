e7d85229bb066ddb43707e04e5e83cf42d7c2357
whitelotus718 <70615488+whitelotus718@users.noreply.github.com>
2021-01-22 13:25:57 -0500

    error handling

--- app.js
+++ app.js
@@ -8,4 +8,41 @@ app.set('view engine', 'pug');
 app.use(morgan('dev'));
 app.use(routes); // is this being used for something other than a middleware?
 
+app.use((req,res, next) => {
+    const err = new Error('The requested pge couldn\'t be found.');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    if (process.env.NODE_ENV === 'production') {
+
+    } else {
+        console.error(err)
+    }
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    if (err.status === 404) {
+        res.status(404);
+        res.render('page-not-found', {
+            title: 'Page Not Found',
+        });
+    }else {
+        next(err);
+    }
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500); // what is the pipes doing?
+    const isProduction = proces.env.NODE_ENV === 'production';
+    res.render('error', {
+        title: 'Server Error',
+        message: isProduction ? null : err.message, 
+        stack: isProduction ? null : err.stack,
+    });
+});
+
+
 module.exports = app;
